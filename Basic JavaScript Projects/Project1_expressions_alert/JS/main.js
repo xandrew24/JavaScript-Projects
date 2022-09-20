ed1b64f193665e3e3055e1f33e850ceac3f9580b
Andrew Thom <drewbee1@comcast.net>
2022-09-19 20:53:26 -0700

    Assignment 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,25 @@
+var A ='This is a string';
+window.alert(A);
+document.write('Hello, World!');                // showing how to use document.write
+document.write(A);
+
+
+document.write("Lisa told Bart, \"Knock it off, Bart! or ill tell dad!\"<br>\
+\"Eat my shorts!\" Bart responded.");
+
+
+document.write("\"Be who you are and say what you feel,"    //Escape Character
++ " because those who mind don\'t matter and those who matter don\'t mind.\""
++ "-Dr. Seuss");
+
+var B = "Hello" + "World"                           //Concatenating a String
+document.write(B);
+
+var Family = "The Thoms", Dad = "Jason", Mom = "Gabby";     //Multiple variables
+document.write(Dad);
+
+document.write(3 + 3);            
+
+var Sent1 = "This is the begininng of the string";      //this is me concatenating the Sent poritons
+var Sent2 = " and this is the end of the string";
+document.write(Sent1 + Sent2);
